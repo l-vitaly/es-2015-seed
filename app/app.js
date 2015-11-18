eb14d81c84276f9e2798588899aa147a35cace6c
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-11-18 14:54:18 +0300

    Refactoring add templates module

--- app/app.js
+++ app/app.js
@@ -3,16 +3,14 @@ import coreModule from './core/core.module';
 import statesModule from './states/states.module';
 import layoutsModule from  './layouts/layouts.module';
 import modulesModule from  './modules/modules.module';
+import templatesModule from './templates';
 
-let appModulePromise = new Promise(function(resolve) {
-  angular.module('app', vendors.concat([
-    coreModule.name,
-    statesModule.name,
-    layoutsModule.name,
-    modulesModule.name
-  ]));
+let moduleNames = [
+  templatesModule.name,
+  coreModule.name,
+  statesModule.name,
+  layoutsModule.name,
+  modulesModule.name
+];
 
-  resolve(angular.module('app'));
-});
-
-export default appModulePromise;
+export default angular.module('app', vendors.concat(moduleNames));
