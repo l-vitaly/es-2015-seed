869f21460f90e11253f8578a2c2b9d809c5442a8
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-11-18 14:56:03 +0300

    Add build template cache

--- dev/gulp/tasks/build-html.js
+++ dev/gulp/tasks/build-html.js
@@ -1,11 +1,20 @@
 var $ = require('gulp-load-plugins')();
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
 var paths = require('../paths');
 var options = require('../options');
 
-gulp.task('build-html', function () {
+/**
+ * Build html templates cache module
+ */
+gulp.task('build-html', function() {
   return gulp.src(paths.html)
-    .pipe($.changed(paths.html[0], {extension: '.html'}))
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe($.minifyHtml(options.minifyHtml))
+    .pipe($.angularTemplatecache({
+      moduleSystem: 'ES6',
+      standalone: true
+    }))
     .pipe(gulp.dest(paths.output.html));
 });
