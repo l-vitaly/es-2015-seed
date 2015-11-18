45062275aacc88013a2b7f66b2d4bf290728a3af
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-11-18 14:55:02 +0300

    Remove interceptor

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -1,15 +1,4 @@
 export function coreConfig(httpProvider, locationProvider) {
-  httpProvider.interceptors.push(function() {
-    return {
-      'request': function(config) {
-        if (/.(html+)$/gi.test(config.url)) {
-          config.url = 'dist/' + config.url;
-        }
-        return config;
-      }
-    };
-  });
-
   httpProvider.defaults.headers.delete = {'Content-Type': 'application/json'};
 
   locationProvider.html5Mode({
