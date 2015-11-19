937a18154ffb5c3e6d1f5b7d7571ba49225b84ff
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-11-20 01:21:44 +0300

    Fix build templates

--- dev/gulp/tasks/build.js
+++ dev/gulp/tasks/build.js
@@ -5,12 +5,12 @@ var runSequence = require('run-sequence');
  * Run all tasks needed for a build in defined order
  */
 gulp.task('build', function (callback) {
-
   runSequence(
     'clean',
     'lint',
     ['copy-fonts', 'copy-images', 'copy-assets'],
-    ['build-html', 'build-sass', 'build-js', 'build-index'],
+    ['build-html', 'build-sass', 'build-index'],
+    'build-js',
     callback
   );
 });
